3cfb301908fc21693c6e12889735ba0222f6e987
hooman_backend <homan8995@gmail.com>
2025-12-15 00:22:46 +0330

    edit api routers

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/admin')->group(function () {
     Route::post('/users/', [\App\Http\Controllers\Api\Admin\UserConutroller::class, 'store'])->name('admin.users.store');
     Route::get('/mail/{id}', [\App\Http\Controllers\Api\Admin\UserConutroller::class, 'mail'])->name('admin.users.mail');
-    Route::post('/checkmail/', [\App\Http\Controllers\Api\Admin\UserConutroller::class, 'checkmail'])->name('admin.users.checkmail');
+
 });
